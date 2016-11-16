d2055ece9c4b1a3304c7ceeaf9058c1468240cf5
yemaofpa <yemao5@126.com>
2016-11-16 11:48:18 +0800

    leetcode-292

--- leetcode/src/main/java/algorithms/leetcode/_292_NimGame.java
+++ leetcode/src/main/java/algorithms/leetcode/_292_NimGame.java
@@ -1,9 +1,46 @@
 package algorithms.leetcode;
 
 /**
+ * <p>你和你的朋友玩一个游戏：桌子上有一堆石头，每次可以拿走1-3个，最后一次拿走剩下所有石头的人获胜。<br>
+ * 你可以先拿。
+ * <p>写一个函数判断，在给定石头总个数的情况下，你是否可以必定获胜。
+ *
  * @author yemaofpa on 2016/11/14.
  */
 public class _292_NimGame {
 
-
+    /**
+     * <P> <b>总结：</b><br>
+     * 1. 通过简单枚举找到规律（可一行代码解决问题）<br>
+     * 2. 关联斐波那契数列
+     * <P> <b>优化：</b><br>
+     * 1. 除了枚举发现规律外，按正常逻辑，算法类似斐波那契数列<br>
+     * 2. 采用非递归算法，降低复杂度
+     *
+     * @param n 石头总个数
+     * @return 先手是否可以必胜
+     * @see <a href="https://leetcode.com/problems/nim-game">nim-game</a>
+     */
+    public boolean isMustWin(int n) {
+        if (n == 1 || n == 2 || n == 3) {
+            return true;
+        }
+        int i = 4;
+        boolean win_3 = true;
+        boolean win_2 = true;
+        boolean win_1 = true;
+        // 当i>=4时，先手拿不完所有的石头，自己拿完后轮到对方拿
+        // 第i次是否能取胜，取决于轮到对方拿时，对方能否能赢
+        // 如果自己拿1-3个三种情况下，有任意一种情况对方不能赢，则自己可以赢
+        boolean wini = !win_3 || !win_2 || !win_1;
+        while (i < n) {
+            i++;
+            // 注意赋值顺序，从低位到高位
+            win_3 = win_2;
+            win_2 = win_1;
+            win_1 = wini;
+            wini = !win_3 || !win_2 || !win_1;
+        }
+        return wini;
+    }
 }
--- leetcode/src/test/java/algorithms/leetcode/Test_292_NimGame.java
+++ leetcode/src/test/java/algorithms/leetcode/Test_292_NimGame.java
@@ -1,9 +1,28 @@
 package algorithms.leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
 /**
  * @author yemaofpa on 2016/11/14.
  */
 public class Test_292_NimGame {
 
+    private _292_NimGame nimGame = new _292_NimGame();
 
+    @Test
+    public void test() {
+        int n = new Random().nextInt(Integer.MAX_VALUE);
+        System.out.println("test n=" + n);
+        Assert.assertEquals(nimGame.isMustWin(n), isMustWin(n));
+    }
+
+    /**
+     * 练习非递归实现
+     */
+    private boolean isMustWin(int n) {
+        return n%4 != 0;
+    }
 }
