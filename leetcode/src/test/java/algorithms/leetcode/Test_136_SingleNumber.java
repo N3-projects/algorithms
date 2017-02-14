a6b9efb17d7c36f4c70d68cb7a65be9070672bd1
yemaofpa <yemao5@126.com>
2017-02-14 12:02:19 +0800

    leetcode-136

---
+++ leetcode/src/test/java/algorithms/leetcode/Test_136_SingleNumber.java
@@ -0,0 +1,24 @@
+package algorithms.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
+/**
+ * @author yemaofpa on 2016/11/14.
+ */
+public class Test_136_SingleNumber {
+
+    private _136_SingleNumber singleNumber = new _136_SingleNumber();
+
+    @Test
+    public void test() {
+        int[] nums = new int[21];
+        Assert.assertEquals(singleNumber.singleNumber(nums), singleNumber(nums));
+    }
+
+    private int singleNumber(int[] nums) {
+        return 0;
+    }
+}
